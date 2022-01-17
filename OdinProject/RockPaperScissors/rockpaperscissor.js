dfcd22a7aadc19b4cff344279428ad02b9ea7a8d
Justin Edelman <justinmedelman@gmail.com>
2022-01-16 21:41:16 -0500

    commit rockpaperscissors files

--- OdinProject/RockPaperScissors/rockpaperscissor.js
+++ OdinProject/RockPaperScissors/rockpaperscissor.js
@@ -10,7 +10,7 @@ function getWeapon(){
 
 let compWeapon = function(){
     let compNumber = Math.floor(Math.random() * 3);
-    console.log(compNumber);
+    //console.log(compNumber);
     switch (compNumber) {
         case 0: return 'rock';
         case 1: return 'scissor';
@@ -18,31 +18,41 @@ let compWeapon = function(){
     }
    }
 
+   let compCount = 0;
+   let userCount = 0;
+   let tieCount = 0;
 
 function warWinner(){
     let compFunction = compWeapon();
     let userWeapon = getWeapon();
-    let compCount = 0;
-    let userCount = 0;
-    let tieCount = 0;
+    let winCount = 2;  //set number needed to win here
     let winnerElement = document.getElementById('results');
     document.getElementById('computer').innerHTML = `Computer weapon = ${compFunction}`;
+    if(userCount == winCount){
+        winnerElement.textContent = `You win with ${winCount} points!`;
+        document.getElementById("rockpaperscissorForm").style.display = "none";
+        document.getElementById("userWinner").style.fontWeight = "600";
+
+    } else if (compCount == winCount){
+        winnerElement.textContent = `Computer wins with ${winCount} points!`;
+        document.getElementById("rockpaperscissorForm").style.display = "none";
+        document.getElementById("computerWinner").style.fontWeight = "600";
+    } else {
     if (compFunction === userWeapon){
         winnerElement.innerHTML = 'Its a tie!';
-        tieCount ++ ;
-    } else if (userWeapon === 'rock' && compFunction === 'scissor'){
+        tieCount += 1;
+        document.getElementById("tieCount").textContent = tieCount;
+    } else if ((userWeapon === 'rock' && compFunction === 'scissor')||
+               (userWeapon === 'paper' && compFunction === 'rock')||
+               (userWeapon === 'scissor' && compFunction === 'paper')){
         winnerElement.innerHTML = `You win, ${userWeapon} beats ${compFunction}`;
-        userCount++;
-    }else if (userWeapon === 'scissor' && compFunction === 'paper'){
-        winnerElement.innerHTML = `You win, ${userWeapon} beats ${compFunction}`;
-        userCount++;
-    }else if (userWeapon === 'paper' && compFunction === 'rock'){
-        winnerElement.innerHTML = `You win, ${userWeapon} beats ${compFunction}`;
-        userCount++;
+        userCount += 1;
+        document.getElementById("userWinCount").textContent = userCount;
     } else {
         winnerElement.innerHTML = `Computer wins, ${compFunction} beats ${userWeapon}`;
-        compCount++;
+        compCount += 1;
+        document.getElementById("compWinCount").textContent = compCount;
     }
 }
-
+}
 
